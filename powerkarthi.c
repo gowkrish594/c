a3692558e66e55546f00b287666b71d8dc8b2771
gowkrish594 <35992516+gowkrish594@users.noreply.github.com>
2018-02-09 16:10:36 +0530

    Create powerkarthi.c

---
+++ powerkarthi.c
@@ -0,0 +1,15 @@
+
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+    int num1, num2;
+    cin >> num1 >> num2;
+    int answer = num1 + num2;
+ 
+    
+    cout<< answer; 
+ 
+    return 0;
+    getch();
+}
