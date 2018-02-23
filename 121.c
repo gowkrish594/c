deb7b44e56e71560eb87d0ce351b4a17481483b9
gowkrish594 <35992516+gowkrish594@users.noreply.github.com>
2018-02-23 15:45:54 +0530

    Create 121.c

---
+++ 121.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#icllude<conio.h>
+
+int main(void) {
+	int n,sum=0,i;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);	
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+	}
+	int avg=sum/n;
+	printf("%d",avg);
+	
+	return 0;
+  getch();
+}
