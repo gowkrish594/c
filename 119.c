81ec4472b473379af563daff2a45792b26030a9f
gowkrish594 <35992516+gowkrish594@users.noreply.github.com>
2018-02-23 15:41:08 +0530

    Create 119.c

---
+++ 119.c
@@ -0,0 +1,12 @@
+       #include<stdio.h>
+       #include<conio.h>
+            void main()
+            {
+            	int n=10,s,i;
+            	for(i=0;i<n+1;i++)
+            	{
+            	//	s=n+1;
+            		printf("%d",i);
+            	}
+            }
+            getch();
