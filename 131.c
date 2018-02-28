1b06359416d830f67116b88e004c48e63605760d
gowkrish594 <35992516+gowkrish594@users.noreply.github.com>
2018-02-28 14:05:45 +0530

    Create 131.c

---
+++ 131.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(void) {
+	int i,j;
+	scanf("%d %d",&i,&j);
+	i=i^j;
+	k=i^j;
+	i=i^j;
+	printf("%d %d",i,j);
+	return 0;
+}getch()
