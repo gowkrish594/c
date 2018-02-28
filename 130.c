48229cfe4fd0140d92492c40365254290be22235
gowkrish594 <35992516+gowkrish594@users.noreply.github.com>
2018-02-28 14:04:04 +0530

    Create 130.c

---
+++ 130.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+  int a,b,temp;
+  clrscr();
+  scanf("%d %d",&a,&b);
+  temp=a;
+  a=b;
+  b=temp;
+  printf("%d %d",a,b);
+  
+return 0;
+getch();
+}
